cc976d2a1e624034499324da0c4ada56bd7ee443
Alex Yoshida <a3yoshid@uwaterloo.ca>
2024-03-28 13:19:41 -0400

    project setup

---
+++ YoshMusic/App/YoshMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoshMusicApp.swift
+//  YoshMusic
+//
+//  Created by Alex Yoshida on 2024-03-28.
+//
+
+import SwiftUI
+
+@main
+struct YoshMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainScreen()
+        }
+    }
+}
