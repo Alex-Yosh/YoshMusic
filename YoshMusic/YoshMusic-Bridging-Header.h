b8dd0aec946fb68ce9529eb8f02eaa3b5a563a7d
Alex Yoshida <a3yoshid@uwaterloo.ca>
2024-04-02 17:19:46 -0400

    added frame to root. made the project buildable

--- YoshMusic/YoshMusic-Bridging-Header.h
+++ YoshMusic/YoshMusic-Bridging-Header.h
@@ -4,10 +4,10 @@
 //
 //  Created by Alex Yoshida on 2024-04-02.
 //
-#import <SpotifyiOS/SpotifyiOS.h>
 
 #ifndef YoshMusic_Bridging_Header_h
 #define YoshMusic_Bridging_Header_h
+#import <SpotifyiOS/SpotifyiOS.h>
 
 
 #endif /* YoshMusic_Bridging_Header_h */
