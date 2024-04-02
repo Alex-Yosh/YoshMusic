05552f729d975542d35af194fe15bff8f4876ba4
Alex Yoshida <a3yoshid@uwaterloo.ca>
2024-04-02 17:07:54 -0400

    added framework and constants

---
+++ YoshMusic/YoshMusic-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  YoshMusic-Bridging-Header.h
+//  YoshMusic
+//
+//  Created by Alex Yoshida on 2024-04-02.
+//
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#ifndef YoshMusic_Bridging_Header_h
+#define YoshMusic_Bridging_Header_h
+
+
+#endif /* YoshMusic_Bridging_Header_h */
